95c3a45e33894c604d006c2dc2814f8d49379e2c
Youngjin Lim <yjlim@clamos.io>
2021-03-08 23:10:09 +0900

    update.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = true;
   runApp(MyApp());
 }
 
@@ -17,41 +15,54 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHome extends StatelessWidget {
-  Widget _buildImageColumn() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black26,
-      ),
-      child: Column(
-        children: [
-          _buildImageRow(1),
-          _buildImageRow(3),
-        ],
-      ),
+  static final bool showGrid = false;
+
+  Widget _buildGrid() {
+    return GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridTileList(30),
     );
   }
 
-  Widget _buildImageRow(int imageIndex) {
-    return Row(
+  List<Container> _buildGridTileList(int count) {
+    return List.generate(count,
+        (index) => Container(child: Image.asset('lib/images/pic${index}.jpg')));
+  }
+
+  Widget _buildList() {
+    return ListView(
       children: [
-        _buildDecoratedImage(imageIndex),
-        _buildDecoratedImage(imageIndex + 1),
+        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+        _tile('United Artists Stonestown Twin', '501 Buckingham Way', Icons.theaters),
+        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+        Divider(),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+        _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+        _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+        _tile('La Ciccia', '291 30th St', Icons.restaurant),
       ],
     );
   }
 
-  Widget _buildDecoratedImage(int imageIndex) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 10,
-            color: Colors.black38,
-          ),
-          borderRadius: const BorderRadius.all(const Radius.circular(8)),
+  ListTile _tile(String title, String subtitle, IconData icon) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
         ),
-        margin: const EdgeInsets.all(4),
-        child: Image.asset('lib/images/pic$imageIndex.jpg'),
+      ),
+      subtitle: Text(subtitle),
+      leading: Icon(
+        icon,
+        color: Colors.blue[500],
       ),
     );
   }
@@ -65,11 +76,9 @@ class MyHome extends StatelessWidget {
           tooltip: 'Navigation Menu',
           onPressed: null,
         ),
-        title: Text('Exaple Title'),
+        title: Text('Flutter Layout Demo'),
       ),
-      body: Center(
-        child: _buildImageColumn(),
-      ),
+      body: Center(child: showGrid ? _buildGrid() : _buildList()),
     );
   }
 }
